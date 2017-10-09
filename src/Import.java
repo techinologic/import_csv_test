3ab15c7217abb0ee81c43153994743e6887d8d54
Paolo T. Inocencion <paoloinocencion@gmail.com>
2017-10-08 20:47:17 -0400

    update. counted 1 circle.

--- src/Import.java
+++ src/Import.java
@@ -5,32 +5,20 @@ import java.util.Scanner;
 class ParseCSVLineByLine {
     static String xStrPath;
     static int[][] myArray;
-    static int[][] x_arr = new int[64][16];
-    static int[][] y_arr = new int[64][16];
+
     static int tolerance = 400;
 
-    static int startingY;
-    static int startingX;
-
-    static int endingY;
-    static int endingX;
-
-
     static void setUpMyCSVArray() {
-        myArray = new int[2160][2560];
+        myArray = new int[300][300];
         Scanner scanner;
 
         int rowc = 0;
-        int row = 0;
-        int colc = 0;
-        int col = 0;
-        String InputLine;
-        double xnum = 0;
         String xfileLocation;
-        xfileLocation = "C:\\Users\\Paopao\\IdeaProjects\\import_csv_test\\aas2.csv";
+        xfileLocation = "C:\\Users\\Paopao\\IdeaProjects\\import_csv_test\\aas.csv";
         //xfileLocation = "/Users/Paolo/IdeaProjects/import_csv_test/25mm.csv";
 
         System.out.println("Array loaded.");
+        System.out.println("Length: " + myArray.length);
 
         try {
             //set up scanner
@@ -50,7 +38,6 @@ class ParseCSVLineByLine {
         }
     }
 
-
     public static boolean detectCircle(int x0, int y0, int radius, int tolerance) {
         int x = radius - 1;
         int y = 0;
@@ -63,7 +50,6 @@ class ParseCSVLineByLine {
         while (x >= y) {
 
             if (myArray[(x0 + x)][(y0 + y)] >= tolerance) { //1
-                vote += 1;
             }
             if (myArray[(x0 + y)][(y0 + x)] >= tolerance) { //2
                 vote += 1;
@@ -117,7 +103,6 @@ class ParseCSVLineByLine {
 
     }
 
-
     public static void printValues(int[][] ar) {
         int counter = 0;
         for (int i = 0; i < myArray.length; i++) {
@@ -135,144 +120,83 @@ class ParseCSVLineByLine {
         }
     }
 
-    public static int getRadius(int[][] array) {
-        int r = 0;
-        int circleTopY = 0;
-        int circleBottomY = 0;
-
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                if (array[i][j] >= tolerance) {
-                    circleTopY = i;
-                }
-            }
-        }
-
-        for (int i = array.length - 1; i > 0; i--) {
-            for (int j = 0; j < array.length; j++) {
-                if (array[i][j] >= tolerance) {
-                    circleBottomY = i + 1;
-                    break;
-                }
-            }
-        }
-
-        System.out.println("first row of circle: " + circleTopY);
-        System.out.println("last row of circle: " + circleBottomY);
-        return (circleTopY - circleBottomY) / 2;
-    }
-
-    public static int getCircleThickness(int[][] array) {
-
-        int start_x = startingX;
-        int start_y = startingY;
-
-        while (array[start_y][start_x] >= tolerance) {
-            start_y += 1;
-        }
-        System.out.println("Ending X, Y: " + start_y + ", " + start_x);
-
-        while (array[start_y][start_x] <= tolerance) {
-
-        }
-        return start_y - startingY;
-    }
-
-
-    public static int getTopY(int[][] array) {
-
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                if (array[i][j] >= tolerance) {
-                    startingY = i;
-                    startingX = j;
-                    System.out.println("Starting X, Y: " + startingY + ", " + startingX);
-                    return -1;
-                }
-            }
-        }
-        return -1;
-    }
-
-    public static int getBottom(int[][] array) {
-
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                if (array[i][j] >= tolerance) return i;
-
-            }
-        }
-        return -1;
-    }
 
     public static int searchCircle(int r, int maxRadius) {
         int count = 0;
         int radius = r;
+        int thickness = 2;
 
-        int circleFoundX = 0;
-        int circleFoundY = 0;
-        int sameCircle = 0;
+        int counter = 0;
+
+
 
         //while (radius != maxRadius) {
 
-        int x45 = (int) (126 + (radius * Math.cos(45/(180/Math.PI))));
-        int y45 = (int) (133 + (radius * Math.sin(45/(180/Math.PI))));
 
-        double x135 = 126 + (radius * Math.cos(135/(180/Math.PI)));
-        double y135 = 133 + (radius * Math.sin(135/(180/Math.PI)));
+        for (int i = radius + 2; i < myArray.length - radius - 1; i++) { //x
+            for (int j = radius + 2; j < myArray[myArray.length-1].length - radius - 1; j++) { //y
 
-        double x225 = 126 + (radius * Math.cos(225/(180/Math.PI)));
-        double y225 = 133 + (radius * Math.sin(225/(180/Math.PI)));
+                int x45 = (int) Math.round(i + (radius * Math.cos(45/(180/Math.PI))));
+                int y45 = (int) Math.round(j + (radius * Math.sin(45/(180/Math.PI))));
 
-        double x315 = 126 + (radius * Math.cos(315/(180/Math.PI)));
-        double y315 = 133 + (radius * Math.sin(315/(180/Math.PI)));
+                int x120 = (int) Math.round(i + (radius * Math.cos(120/(180/Math.PI))));
+                int y120 = (int) Math.round(j + (radius * Math.sin(120/(180/Math.PI))));
+
+                int x135 = (int) Math.round(i + (radius * Math.cos(135/(180/Math.PI))));
+                int y135 = (int) Math.round(j + (radius * Math.sin(135/(180/Math.PI))));
+
+                int x150 = (int) Math.round(i + (radius * Math.cos(150/(180/Math.PI))));
+                int y150 = (int) Math.round(j + (radius * Math.sin(150/(180/Math.PI))));
+
+                int x225 = (int) Math.round(i + (radius * Math.cos(225/(180/Math.PI))));
+                int y225 = (int) Math.round(j + (radius * Math.sin(225/(180/Math.PI))));
+
+                int x315 = (int) Math.round(i + (radius * Math.cos(315/(180/Math.PI))));
+                int y315 = (int) Math.round(j + (radius * Math.sin(315/(180/Math.PI))));
 
 
-        for (int i = radius + 1; i < myArray.length - radius - 1; i++) { //x
-            for (int j = radius + 1; j < myArray.length - radius - 1; j++) { //y
+                counter+=1;
+
                 if ((myArray[i + radius][j] > tolerance)
-                        && (myArray[i + radius + 1][j] > tolerance)
-                        && (myArray[i + radius - 1][j] > tolerance)
+                        && (myArray[i + radius + thickness][j] > tolerance) // south
+                        && (myArray[i + radius - thickness][j] > tolerance)
 
                         && (myArray[i - radius][j] > tolerance)
-                        && (myArray[i - radius + 1][j] > tolerance)
-                        && (myArray[i - radius - 1][j] > tolerance)
+                        && (myArray[i - radius + thickness][j] > tolerance) //north
+                        && (myArray[i - radius - thickness][j] > tolerance)
 
                         && (myArray[i][j + radius] > tolerance)
-                        && (myArray[i][j + radius + 1] > tolerance)
-                        && (myArray[i][j + radius - 1] > tolerance)
+                        && (myArray[i][j + radius + thickness] > tolerance) // east
+                        && (myArray[i][j + radius - thickness] > tolerance)
 
                         && (myArray[i][j - radius] > tolerance)
-                        && (myArray[i][j - radius + 1] > tolerance)
-                        && (myArray[i][j - radius - 1] > tolerance)
+                        && (myArray[i][j - radius + thickness] > tolerance) // west
+                        && (myArray[i][j - radius - thickness] > tolerance)
 
                         && (myArray[x45][y45] > tolerance)
-                        && (myArray[(int) x135][(int) y135] > tolerance)
-                        && (myArray[(int) x225][(int) y225] > tolerance)
-                        && (myArray[(int) x315][(int) y315] > tolerance)
-                        && (circleFoundY==0)
-                        && (circleFoundX==0)) { // if statement open
-
-                    circleFoundY = 1;
-                    circleFoundX = 1;
+//                        && (myArray[x120][y120] > tolerance)
+                        && (myArray[x135][y135] > tolerance)
+//                        && (myArray[x150][y150] > tolerance)
+                        && (myArray[x225][y225] > tolerance)
+                        && (myArray[x315][y315] > tolerance))
+                { // if statement open
 
                     count+=1;
+                    System.out.println("Center: " + i + " " +j);
 
 
-
-                }
+                } // if close
 
 
 //                    System.out.println("x45: " + x45 + " " + "y45: " + y45);
 //                    System.out.println("x135: " + x135 + " " + "y135: " + y135);
 //                    System.out.println("x225: " + x225 + " " + "y225: " + y225);
 //                    System.out.println("x315: " + x315 + " " + "y315: " + y315);
-                circleFoundY = 0;
             }
-            circleFoundX = 0;
         }
 
         System.out.println("radius: " + radius);
+        System.out.println("points searched: " + counter);
         return count;
     }
 
@@ -281,19 +205,39 @@ class ParseCSVLineByLine {
     public static void main(String[] args) throws Exception {
 
         setUpMyCSVArray();
-        //printValues(myArray);
-        //traverseArrayDiagonally(myArray);
 
-        //detectCircle(129,129,93, tolerance);
+        System.out.println("\nCircles found: " + searchCircle(91, 96));
+        System.out.println("\nSearch completed");
 
-        //System.out.println("Radius: " + getRadius(myArray));
-        //System.out.println("Circle thickness: " + getCircleThickness(myArray));
+        int testRadius = 9;
+        int xTest = 256;
+        int yTest = 216;
 
-        //testArray(myArray);
+        double x45 = Math.round(xTest + (testRadius * Math.cos(45/(180/Math.PI))));
+        double y45 = Math.round(yTest + (testRadius * Math.sin(45/(180/Math.PI))));
 
-        getTopY(myArray);
-        System.out.println("Circles found: " + searchCircle(91, 96));
-        System.out.println("Search completed");
+        double x120 = (xTest + (testRadius * Math.cos(120/(180/Math.PI))));
+        double y120 = (yTest + (testRadius * Math.sin(120/(180/Math.PI))));
+        double x150 = (xTest + (testRadius * Math.cos(150/(180/Math.PI))));
+        double y150 = (yTest + (testRadius * Math.sin(150/(180/Math.PI))));
+
+        double x135 = Math.round(xTest + (testRadius * Math.cos(135/(180/Math.PI))));
+        double y135 = Math.round(yTest + (testRadius * Math.sin(135/(180/Math.PI))));
+
+        double x225 = Math.round(xTest + (testRadius * Math.cos(225/(180/Math.PI))));
+        double y225 = Math.round(yTest + (testRadius * Math.sin(225/(180/Math.PI))));
+
+        double x315 = Math.round(xTest + (testRadius * Math.cos(315/(180/Math.PI))));
+        double y315 = Math.round(yTest + (testRadius * Math.sin(315/(180/Math.PI))));
+
+
+        System.out.println(x45 + " " + y45);
+        //System.out.println(x120 + " " + y120);
+        //System.out.println(x150 + " " + y150);
+        System.out.println(x135 + " " + y135);
+        System.out.println(x225 + " " + y225);
+        System.out.println(x315 + " " + y315);
+
 
     }
 
